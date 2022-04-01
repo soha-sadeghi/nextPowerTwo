d34482c66502375b436dcc80d22ade4f0a9edf62
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-04-01 11:53:06 +0430

    next Power Two

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.io.OutputStream;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int output ;
+        int n ;
+        for (output=1;  output*output< input ;output++) {
+
+                }
+        System.out.println(output * output);
+        }
+    }
+
